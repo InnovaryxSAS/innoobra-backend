c5157425dd2357c9db42dcebebf1df084486ea81
Camilo Martinez <innovaryx>
2025-07-03 17:20:39 -0500

    Cambiar enrutamiento de .jar

--- infra/dev/variables.tf
+++ infra/dev/variables.tf
@@ -9,31 +9,31 @@ variable "lambdas" {
   default = {
     create_company = {
       handler   = "com.lambdas.handler.CreateCompanyHandler::handleRequest"
-      jar_path  = "${path.module}/dev/functions/company/lambdas-1.0-SNAPSHOT.jar"
+      jar_path  = "dev/functions/company/lambdas-1.0-SNAPSHOT.jar"
       route_key = "POST /company"
       method    = "POST"
     },
     get_companies = {
       handler   = "com.lambdas.handler.GetCompaniesHandler::handleRequest"
-      jar_path  = "${path.module}/dev/functions/company/lambdas-1.0-SNAPSHOT.jar"
+      jar_path  = "dev/functions/company/lambdas-1.0-SNAPSHOT.jar"
       route_key = "GET /company"
       method    = "GET"
     },
     get_companiesID = {
       handler   = "com.lambdas.handler.GetCompanyByIdHandler::handleRequest"
-      jar_path  = "${path.module}/dev/functions/company/lambdas-1.0-SNAPSHOT.jar"
+      jar_path  = "dev/functions/company/lambdas-1.0-SNAPSHOT.jar"
       route_key = "GET /company/{id}"
       method    = "GET"
     },
     update_company = {
       handler   = "com.lambdas.handler.UpdateCompanyHandler::handleRequest"
-      jar_path  = "${path.module}/dev/functions/company/lambdas-1.0-SNAPSHOT.jar"
+      jar_path  = "dev/functions/company/lambdas-1.0-SNAPSHOT.jar"
       route_key = "PUT /company/{id}"
       method    = "PUT"
     },
     delete_company = {
       handler   = "com.lambdas.handler.DeleteCompanyHandler::handleRequest"
-      jar_path  = "${path.module}/dev/functions/company/lambdas-1.0-SNAPSHOT.jar"
+      jar_path  = "dev/functions/company/lambdas-1.0-SNAPSHOT.jar"
       route_key = "DELETE /company/{id}"
       method    = "DELETE"
     },
