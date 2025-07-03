8337f686c33c42d276c5f401a65642790bef3c69
Camilo Martinez <innovaryx>
2025-07-03 17:03:34 -0500

    Cambiar enrutamiento de .jar

--- infra/dev/variables.tf
+++ infra/dev/variables.tf
@@ -8,16 +8,35 @@ variable "lambdas" {
   }))
   default = {
     create_company = {
-      handler   = "com.innobra.lambda.Handler::handleRequest"
+      handler   = "com.lambdas.handler.CreateCompanyHandler::handleRequest"
       jar_path  = "${path.module}/company/createCompanyHandler.jar"
       route_key = "POST /company"
       method    = "POST"
     },
     get_companies = {
-      handler   = "com.tubanco.company.getCompaniesHandler::handleRequest"
+      handler   = "com.lambdas.handler.GetCompaniesHandler::handleRequest"
       jar_path  = "${path.module}/company/getCompaniesHandler.jar"
       route_key = "GET /company"
-      method    = "POST"
-    }
+      method    = "GET"
+    },
+    get_companiesID = {
+      handler   = "com.lambdas.handler.GetCompanyByIdHandler::handleRequest"
+      jar_path  = "${path.module}/company/createCompanyHandler.jar"
+      route_key = "GET /company/{id}"
+      method    = "GET"
+    },
+    update_company = {
+      handler   = "com.lambdas.handler.UpdateCompanyHandler::handleRequest"
+      jar_path  = "${path.module}/company/createCompanyHandler.jar"
+      route_key = "PUT /company/{id}"
+      method    = "PUT"
+    },
+    delete_company = {
+      handler   = "com.lambdas.handler.DeleteCompanyHandler::handleRequest"
+      jar_path  = "${path.module}/company/createCompanyHandler.jar"
+      route_key = "DELETE /company/{id}"
+      method    = "DELETE"
+    },
+    
   }
 }
