0204e7284ff956ba22e7dd18222496fb27a3b491
Camilo Martinez <innovaryx>
2025-07-03 15:19:15 -0500

    Implementación de código terraform para diferentes lambdas

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -21,25 +21,9 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-
-
 resource "aws_apigatewayv2_api" "http_api" {
-  name          = "http-api"
+  name          = "http-api-budget"
   protocol_type = "HTTP"
-}
-
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.http_api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.hello.invoke_arn
-  integration_method = "POST"
-  payload_format_version = "2.0"
-}
-
-resource "aws_apigatewayv2_route" "default_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "GET /"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
 resource "aws_apigatewayv2_stage" "default" {
@@ -48,14 +32,52 @@ resource "aws_apigatewayv2_stage" "default" {
   auto_deploy = true
 }
 
-resource "aws_lambda_permission" "api_gw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
-}
-
 output "endpoint_url" {
   value = aws_apigatewayv2_api.http_api.api_endpoint
 }
+
+resource "aws_lambda_function" "lambda" {
+  for_each      = var.lambdas
+  function_name = each.key
+  handler       = each.value.handler
+  runtime       = "java17"
+  filename      = "${path.module}/${each.value.jar_path}"
+  source_code_hash = filebase64sha256("${path.module}/${each.value.jar_path}")
+  role          = aws_iam_role.lambda_exec.arn
+  memory_size   = 128
+  timeout       = 15
+  environment {
+    variables = {
+      ENV = "dev"
+    }
+  }
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  for_each                = var.lambdas
+  api_id                  = aws_apigatewayv2_api.http_api.id
+  integration_type        = "AWS_PROXY"
+  integration_uri         = aws_lambda_function.lambda[each.key].invoke_arn
+  integration_method      = each.value.method
+  payload_format_version  = "2.0"
+}
+
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  for_each   = var.lambdas
+  api_id     = aws_apigatewayv2_api.http_api.id
+  route_key  = each.value.route_key
+  target     = "integrations/${aws_apigatewayv2_integration.lambda_integration[each.key].id}"
+}
+
+
+resource "aws_lambda_permission" "api_gw" {
+  for_each       = var.lambdas
+  statement_id   = "AllowExecutionFromAPIGateway-${each.key}"
+  action         = "lambda:InvokeFunction"
+  function_name  = aws_lambda_function.lambda[each.key].function_name
+  principal      = "apigateway.amazonaws.com"
+  source_arn     = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+
--- infra/dev/variables.tf
+++ infra/dev/variables.tf
@@ -1,3 +1,23 @@
-variable "lambda_function_name" {
-  default = "company"
+variable "lambdas" {
+  description = "Lambda list"
+  type = map(object({
+    handler   = string      
+    jar_path  = string      
+    route_key = string      
+    method    = string      
+  }))
+  default = {
+    create_company = {
+      handler   = "com.innobra.lambda.Handler::handleRequest"
+      jar_path  = "${path.module}/company/createCompanyHandler.jar"
+      route_key = "POST /company"
+      method    = "POST"
+    },
+    get_companies = {
+      handler   = "com.tubanco.company.getCompaniesHandler::handleRequest"
+      jar_path  = "${path.module}/company/getCompaniesHandler.jar"
+      route_key = "GET /company"
+      method    = "POST"
+    }
+  }
 }
