b67b9df253ff972540f8e7eb14611fa6723dc1ad
Camilo Martinez <innovaryx>
2025-07-03 00:07:31 -0500

    Cambio de rol lambda

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role_v3"
+  name = "lambda_exec_role_v4"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
