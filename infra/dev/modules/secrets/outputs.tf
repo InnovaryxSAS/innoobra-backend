796fe1d5591934414923f71f18115182b857c2bf
Camilo Martinez <innovaryx>
2025-07-13 22:26:07 -0500

    Ajuste en secret rds

--- infra/dev/modules/secrets/outputs.tf
+++ infra/dev/modules/secrets/outputs.tf
@@ -2,3 +2,7 @@ output "secret_arn" {
   description = "ARN of the DB credentials secret"
   value       = aws_secretsmanager_secret.db.arn
 }
+output "db_secret_arn" {
+  description = "El ARN del secreto RDS"
+  value       = aws_secretsmanager_secret.db.arn
+}
