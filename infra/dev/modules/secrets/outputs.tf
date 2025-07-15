b83ef187520d14ab245691f2eb8b69a4620e302a
Camilo Martinez <innovaryx>
2025-07-15 17:12:07 -0500

    Eliminación Output de secret de la clase de base de datos

--- infra/dev/modules/secrets/outputs.tf
+++ infra/dev/modules/secrets/outputs.tf
@@ -4,7 +4,3 @@ output "db_secret_arn" {
   value       = aws_secretsmanager_secret.db.arn
 }
 
-output "db_secret_version_id" {
-  description = "Versión actual del secreto"
-  value       = aws_secretsmanager_secret_version.db_version.version_id
-}
