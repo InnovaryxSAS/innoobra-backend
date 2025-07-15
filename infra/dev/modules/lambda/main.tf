f785272f6a67602a36911e090b64a6246c73016f
Camilo Martinez <innovaryx>
2025-07-14 19:28:58 -0500

    Ajuste en refresco del token

--- infra/dev/modules/lambda/main.tf
+++ infra/dev/modules/lambda/main.tf
@@ -37,6 +37,27 @@ resource "aws_iam_role_policy" "lambda_secrets" {
   })
 }
 
+resource "aws_iam_role_policy" "lambda_ec2" {
+  name = "LambdaEC2Access-${var.environment}"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
 ##############################################################################
 # 2) API Gateway HTTP
 ##############################################################################
