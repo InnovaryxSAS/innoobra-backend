9768b7a28991e6d0be8353862e63a7c4d67f03bf
Camilo Martinez <innovaryx>
2025-07-17 13:49:10 -0500

    Permisos para leer secrets desde las lambdas

--- infra/dev/modules/endpoints/main.tf
+++ infra/dev/modules/endpoints/main.tf
@@ -16,11 +16,3 @@ resource "aws_vpc_endpoint" "ssmmessages" {
   private_dns_enabled = true
 }
 
-resource "aws_vpc_endpoint" "secretsmanager" {
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.${var.region}.secretsmanager"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = var.private_subnet_ids
-  security_group_ids  = [var.lambda_sg_id]
-  private_dns_enabled = true
-}
